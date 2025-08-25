f779fbe5f0f929e97b201543c223509c1d8ec1e2
Muhammad Firzak Rizki <firzakrizki@gmail.com>
2025-08-25 23:42:48 +0700

    Netifly Setting

--- vite.config.ts
+++ vite.config.ts
@@ -13,4 +13,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: 'dist'
+  },
+  base: '/', // atau '/' untuk root domain, sesuaikan kalau pakai subpath
 })
